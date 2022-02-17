ceb30405ca83ac2d86bb1dda4934de3bb36a3cd0
lucastsan <lucastsantos1020@gmail.com>
2022-02-17 00:21:23 -0300

    added counter selection bar for delete

--- lib/components/snackbar.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class SnackBarSelection extends SnackBar {
-  SnackBarSelection({required this.selectedItems})
-      : super(content: Text(selectedItems.toString() + ' Items selecionados'));
-
-  final int selectedItems;
-}
--- lib/screens/current_expenses.dart
+++ lib/screens/current_expenses.dart
@@ -5,7 +5,6 @@ import 'package:pocket_finances/components/hero_dialog_route.dart';
 import 'package:pocket_finances/constants.dart';
 import 'package:pocket_finances/models/expense_model.dart';
 import 'package:pocket_finances/screens/add_current_expenses.dart';
-import 'package:pocket_finances/components/snackbar.dart';
 
 class CurrentExpensesPage extends StatefulWidget {
   const CurrentExpensesPage({Key? key}) : super(key: key);
@@ -18,8 +17,6 @@ class _CurrentExpensesPageState extends State<CurrentExpensesPage> {
   final String heroTag = 'add-current-expense';
   late List<Expense> expensesList;
   List<int> _selectedItems = [];
-  late SnackBar snackbar =
-      SnackBarSelection(selectedItems: _selectedItems.length);
 
   @override
   Widget build(BuildContext context) {
@@ -39,81 +36,106 @@ class _CurrentExpensesPageState extends State<CurrentExpensesPage> {
                 ),
               );
             } else {
-              return ListView.builder(
-                  itemCount: snapshot.data?.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      color: (_selectedItems.contains(index))
-                          ? kHighlightColor
-                          : Colors.transparent,
-                      child: ListTile(
-                        onLongPress: () {
-                          if (!_selectedItems.contains(index)) {
-                            setState(() {
-                              ScaffoldMessenger.of(context)
-                                  .showSnackBar(snackbar);
-                              _selectedItems.add(index);
-                              print(_selectedItems);
-                            });
-                          }
-                        },
-                        onTap: () {
-                          if (_selectedItems.isNotEmpty) {
-                            if (_selectedItems.contains(index)) {
-                              setState(() {
-                                _selectedItems
-                                    .removeWhere((element) => element == index);
-                                print(_selectedItems);
-                              });
-                            } else {
-                              setState(() {
-                                _selectedItems.add(index);
-                                print(_selectedItems);
-                              });
-                            }
-                          }
-                        },
-                        title: Text(
-                          snapshot.data![index].name,
-                          style: kBalanceTitleStyle,
-                        ),
-                        subtitle: Text(
-                          snapshot.data![index].date,
-                          style: kInfoValueStyle,
-                        ),
-                        trailing: Text(
-                          snapshot.data![index].value.toString(),
-                          style: kBalanceTitleStyle,
-                        ),
-                      ),
-                    );
-                  });
+              return Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                        itemCount: snapshot.data?.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Container(
+                            color: (_selectedItems.contains(index))
+                                ? kHighlightColor
+                                : Colors.transparent,
+                            child: ListTile(
+                              onLongPress: () {
+                                if (!_selectedItems.contains(index)) {
+                                  setState(() {
+                                    _selectedItems.add(index);
+                                    print(_selectedItems);
+                                  });
+                                }
+                              },
+                              onTap: () {
+                                if (_selectedItems.isNotEmpty) {
+                                  if (_selectedItems.contains(index)) {
+                                    setState(() {
+                                      _selectedItems.removeWhere(
+                                          (element) => element == index);
+                                      print(_selectedItems);
+                                    });
+                                  } else {
+                                    setState(() {
+                                      _selectedItems.add(index);
+                                      print(_selectedItems);
+                                    });
+                                  }
+                                }
+                              },
+                              title: Text(
+                                snapshot.data![index].name,
+                                style: kBalanceTitleStyle,
+                              ),
+                              subtitle: Text(
+                                snapshot.data![index].date,
+                                style: kInfoValueStyle,
+                              ),
+                              trailing: Text(
+                                snapshot.data![index].value.toString(),
+                                style: kBalanceTitleStyle,
+                              ),
+                            ),
+                          );
+                        }),
+                  ),
+                  _selectedItems.length >= 1
+                      ? Container(
+                          padding: EdgeInsets.all(15),
+                          width: double.maxFinite,
+                          height: 80,
+                          color: kMainCard,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(_selectedItems.length.toString() +
+                                  ' Items selecionados'),
+                              ElevatedButton(
+                                onPressed: () {},
+                                child: Icon(Icons.delete),
+                              )
+                            ],
+                          ),
+                        )
+                      : Container(),
+                ],
+              );
             }
           },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        heroTag: heroTag,
-        onPressed: () async {
-          final result = await Navigator.of(context)
-              .push(HeroDialogRoute(builder: (context) {
-            return AddCurrentExpenses(heroTag: heroTag);
-          }));
-          if (result != false) {
-            setState(() {
-              addExpense(result, expensesList);
-              expController.add(1);
-            });
-          }
-        },
-        child: Material(
-          type: MaterialType.transparency,
-          child: Icon(
-            Icons.add,
-            size: 30.0,
-          ),
-        ),
-      ),
+      floatingActionButton: _selectedItems.length == 0
+          ? FloatingActionButton(
+              heroTag: heroTag,
+              onPressed: () async {
+                final result = await Navigator.of(context)
+                    .push(HeroDialogRoute(builder: (context) {
+                  return AddCurrentExpenses(heroTag: heroTag);
+                }));
+                if (result != false) {
+                  setState(() {
+                    addExpense(result, expensesList);
+                    expController.add(1);
+                  });
+                }
+              },
+              child: Material(
+                type: MaterialType.transparency,
+                child: Icon(
+                  Icons.add,
+                  size: 30.0,
+                ),
+              ),
+            )
+          : null,
     );
   }
 
